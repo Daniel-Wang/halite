0a75e190dc257500670ea70b120475c6592f86fa
Agastya Kalra <agastya.kalra@gmail.com>
2016-11-23 03:46:34 -0500

    final_bot

--- runGame.sh
+++ runGame.sh
@@ -5,7 +5,7 @@ export v1="MyExpJavaBot"
 javac "$v1".java
 javac "$v2".java
 ./halite -d "40 40" "java $v1" "java $v2" | tail -5
-./halite -d "30 30" "java $v2" "java $v1" | tail -5
-./halite -d "45 45" "java $v1" "java $v2" "java $v2" "java $v1" | tail -5
+./halite -d "30 30" "java $v1" "java $v2" | tail -5
+./halite -d "45 45" "java $v2" "java $v1" "java $v2" "java $v1" | tail -5
 
 
